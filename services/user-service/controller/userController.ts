3425dd05a31ca95209374761de7179d98543088c
Selasie5 <selasisepenu5@gmail.com>
2025-01-20 00:21:18 +0000

    implement user controller for user service

--- services/user-service/controller/userController.ts
+++ services/user-service/controller/userController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
-import primsa from "../../../common/db.js";
+import prisma from "../../../common/db.js";
 import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
 
+interface CustomRequest extends Request {
+    userId?: string;
+}
+
+const JWT_SECRET = process.env.JWT_SECRET || "default-secret";
+//Handle user registration
 export const register = async(req:Request, res:Response)=>
 {
     const {email, password, firstName, lastName}= req.body;
@@ -10,7 +16,7 @@ export const register = async(req:Request, res:Response)=>
     const hashedPassword =  await bcrypt.hash(password, 10)
 try {
     
-    const user = await primsa.user.create({
+    const user = await prisma.user.create({
         data:{
             email, 
             password:hashedPassword,
@@ -24,4 +30,39 @@ try {
     res.status(500).json({error:"Internal server error"});
 }
     
+}
+
+//This handle user logins
+
+export const login = async(req:Request, res:Response):Promise<void>=>
+{
+    const {email, passowrd}= req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where:{
+                email
+            }
+        })
+        if(!user || !await bcrypt.compare(passowrd, user.password))
+        {
+            res.status(401).json({error:"Invalid user credentials"})
+            
+        }
+    } catch (error) {
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+export const getProfile =async(req: CustomRequest, res:Response):Promise<void> =>{
+ const userId = req.userId;
+ try {
+    const user = await prisma.user.findUnique({
+        where:{
+            id: userId ? parseInt(userId, 10) : undefined
+        }
+    });
+    res.status(200).json(user);
+ } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+ } 
 }
